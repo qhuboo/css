dfd78a5807dfc221bfab71cb893bc172347aa22f
Jose Lopez Renteria <joselukas24@gmail.com>
2024-02-01 21:20:53 -0600

    Done with exercises

--- josh_css_course/module_3/exercises/src/App.jsx
+++ josh_css_course/module_3/exercises/src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import "./components/FrequentlyAskedQuestion/FrequentlyAskedQuestion";
+import FrequentlyAskedQuestion from "./components/FrequentlyAskedQuestion/FrequentlyAskedQuestion";
+import LoginForm from "./components/LoginForm/LoginForm";
+import ContactCard from "./components/ContactCard/ContactCard";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <FrequentlyAskedQuestion
+        question="What does “CSS” stand for?"
+        answer="Cool Styling Strategy"
+      />
+      <LoginForm />
+      <ContactCard
+        avatarSrc="https://courses.joshwcomeau.com/cfj-mats/cat-300px.jpg"
+        name="Mittens"
+        email="meow@gmail.com"
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ josh_css_course/module_3/exercises/src/components/FrequentlyAskedQuestion/FrequentlyAskedQuestion.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+function FrequentlyAskedQuestion({ question, answer }) {
+  return (
+    <Details>
+      <Summary>{question}</Summary>
+      <Answer>{answer}</Answer>
+    </Details>
+  );
+}
+
+const Details = styled.details`
+  font-size: 1rem;
+  color: hsl(0deg 0% 25%);
+  margin: 32px;
+  border: 2px solid;
+  border-radius: 4px;
+`;
+
+const Summary = styled.summary`
+  cursor: pointer;
+  padding: 12px 16px;
+  outline-offset: 5px;
+`;
+
+const Answer = styled.div`
+  background: hsl(0deg 0% 92.5%);
+  border-radius: 0 0 4px 4px;
+  padding: 12px 17px;
+  font-style: italic;
+`;
+
+export default FrequentlyAskedQuestion;
