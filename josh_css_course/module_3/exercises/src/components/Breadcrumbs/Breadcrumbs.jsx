8bbe5112b430c19d3a79c28174aa8f3c56188107
Jose Lopez Renteria <joselukas24@gmail.com>
2024-02-03 10:30:07 -0600

    finished with breadcrumbs

--- josh_css_course/module_3/exercises/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ josh_css_course/module_3/exercises/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -8,14 +8,51 @@ const Breadcrumbs = ({ children }) => {
   );
 };
 
-const Crumb = ({ children }) => {
-  return <li>{children}</li>;
+const Crumb = ({ href, isCurrentPage, children }) => {
+  return (
+    <CrumbWrapper>
+      <CrumbLink
+        href={href}
+        arria-aria-current={isCurrentPage ? "page" : undefined}
+      >
+        {children}
+      </CrumbLink>
+    </CrumbWrapper>
+  );
 };
 
 const BreadcrumbList = styled.ol`
   padding: 0;
   margin: 0;
   list-style-type: none;
+  width: fit-content;
+`;
+
+const CrumbWrapper = styled.li`
+  display: inline;
+  --spacing: 12px;
+
+  &:not(:first-of-type) {
+    margin-left: var(--spacing);
+
+    &::before {
+      content: "";
+      display: inline-block;
+      border-right: 1px solid;
+      transform: rotate(15deg);
+      margin-right: var(--spacing);
+      height: 0.8em;
+    }
+  }
+`;
+
+const CrumbLink = styled.a`
+  color: inherit;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: revert;
+  }
 `;
 
 const BreadCrumb = () => {
@@ -24,7 +61,9 @@ const BreadCrumb = () => {
       <Crumb href="/">Home</Crumb>
       <Crumb href="/living">Living Room</Crumb>
       <Crumb href="/living/couch">Couches</Crumb>
-      <Crumb href="/living/couch/sectional">Sectionals</Crumb>
+      <Crumb href="/living/couch/sectional" isCurrentPage>
+        Sectionals
+      </Crumb>
     </Breadcrumbs>
   );
 };
