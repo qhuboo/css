035b210ca5d69c2d95ef549de6fb2c594bde5c33
Jose Lopez Renteria <joselukas24@gmail.com>
2024-02-08 14:38:18 -0600

    done with progressbar

--- josh_css_course/module_3/workshop/mini-component-library-main/src/components/ProgressBar/ProgressBar.js
+++ josh_css_course/module_3/workshop/mini-component-library-main/src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,40 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper role="progressbar" aria-valuenow={value} size={size}>
+      <Progress role="presentation" value={value} size={size} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  overflow: hidden;
+  border-radius: ${(props) => (props.size === "large" ? "8px" : "4px")};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const Progress = styled.div`
+  width: ${(props) =>
+    props.size === "large"
+      ? `calc((100% - 8px) * ${props.value / 100})`
+      : `calc((100%) * ${props.value / 100})`};
+  margin: ${(props) => (props.size === "large" ? "4px" : 0)};
+  background-color: ${COLORS.primary};
+  height: ${(props) =>
+    props.size === "large" ? "16px" : props.size === "medium" ? "12px" : "8px"};
+  border-radius: ${(props) => {
+    if (props.size === "large") {
+      return props.value === 100 ? "4px" : "4px 0px 0px 4px";
+    } else {
+      return 0;
+    }
+  }};
+`;
 
 export default ProgressBar;
