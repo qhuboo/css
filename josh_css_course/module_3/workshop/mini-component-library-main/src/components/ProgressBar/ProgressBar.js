d5356203d97a53f41bd6928ada806d37049b9427
Jose Lopez Renteria <joselukas24@gmail.com>
2024-02-08 17:08:13 -0600

    none of the calc were necessary

--- josh_css_course/module_3/workshop/mini-component-library-main/src/components/ProgressBar/ProgressBar.js
+++ josh_css_course/module_3/workshop/mini-component-library-main/src/components/ProgressBar/ProgressBar.js
@@ -7,24 +7,32 @@ import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
   return (
-    <Wrapper role="progressbar" aria-valuenow={value} size={size}>
-      <Progress role="presentation" value={value} size={size} />
-    </Wrapper>
+    <>
+      <Wrapper role="progressbar" aria-valuenow={value} size={size}>
+        <VisuallyHidden>{value}%</VisuallyHidden>
+        <BarWrapper>
+          <Bar role="presentation" value={value} size={size} />
+        </BarWrapper>
+      </Wrapper>
+    </>
   );
 };
 
 const Wrapper = styled.div`
-  overflow: hidden;
+  width: 150px;
+  background-color: ${COLORS.transparentGray15};
   border-radius: ${(props) => (props.size === "large" ? "8px" : "4px")};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: ${(props) => (props.size === "large" ? "4px" : 0)};
 `;
 
-const Progress = styled.div`
-  width: ${(props) =>
-    props.size === "large"
-      ? `calc((100% - 8px) * ${props.value / 100})`
-      : `calc((100%) * ${props.value / 100})`};
-  margin: ${(props) => (props.size === "large" ? "4px" : 0)};
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: ${(props) => props.value}%;
   background-color: ${COLORS.primary};
   height: ${(props) =>
     props.size === "large" ? "16px" : props.size === "medium" ? "12px" : "8px"};
