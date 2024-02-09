3a2afc2a208c1a4cdadbec40a0ae4b3d6fd2b04b
Jose Lopez Renteria <joselukas24@gmail.com>
2024-02-08 21:02:11 -0600

    DONE with select

--- josh_css_course/module_3/workshop/mini-component-library-main/src/components/Select/Select.js
+++ josh_css_course/module_3/workshop/mini-component-library-main/src/components/Select/Select.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 
 import { COLORS } from "../../constants";
@@ -6,17 +6,17 @@ import Icon from "../Icon";
 import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
+  const [isFocused, setIsFocused] = useState(false);
   const displayedValue = getDisplayedValue(value, children);
-  const selectRef = useRef(null);
-  const handleWrapperClick = () => {
-    selectRef.current.focus();
-  };
+
   return (
-    <SelectWrapper
-      onClick={handleWrapperClick}
-      value={value}
-      onChange={onChange}
-    >
+    <NewSelect value={value} onChange={onChange} isFocused={isFocused}>
+      <InvisibleSelect
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      >
+        {children}
+      </InvisibleSelect>
       <DisplayValue>{displayedValue}</DisplayValue>
       <Icon
         style={{
@@ -26,12 +26,21 @@ const Select = ({ label, value, onChange, children }) => {
         }}
         id="chevron-down"
       />
-      <InvisibleSelect ref={selectRef}>{children}</InvisibleSelect>
-    </SelectWrapper>
+    </NewSelect>
   );
 };
 
-const SelectWrapper = styled.div`
+const InvisibleSelect = styled.select`
+  border: 1px dashed red;
+  opacity: 0;
+  position: absolute;
+  inset: 0;
+  margin: auto;
+  /* Allow the select to span the full height in Safari */
+  -webkit-appearance: none;
+`;
+
+const NewSelect = styled.div`
   width: max-content;
   background-color: ${COLORS.transparentGray15};
   position: relative;
@@ -42,19 +51,17 @@ const SelectWrapper = styled.div`
   &:hover {
     color: black;
   }
-`;
 
-const InvisibleSelect = styled.select`
-  border: 1px dashed red;
-  opacity: 0;
-  position: absolute;
-  inset: 0;
-  margin: auto;
+  & {
+    outline: ${(props) => props.isFocused && "1px dotted #212121"};
+    outline: ${(props) =>
+      props.isFocused && "5px auto -webkit-focus-ring-color"};
+  }
 `;
 
 const DisplayValue = styled.span`
   color: inherit;
-  margin-right: 24px;
+  margin-right: 20px;
 `;
 
 export default Select;
