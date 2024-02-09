dbc429eac52d0bde25f8521f02e473244e4429f9
Jose Lopez Renteria <joselukas24@gmail.com>
2024-02-08 19:37:50 -0600

    done with select

--- josh_css_course/module_3/workshop/mini-component-library-main/src/components/Select/Select.js
+++ josh_css_course/module_3/workshop/mini-component-library-main/src/components/Select/Select.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 
 import { COLORS } from "../../constants";
 import Icon from "../Icon";
