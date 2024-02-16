ba6c0ed6f4c3f8852aa140d37cbc84491b3df24a
Jose Lopez Renteria <joselukas24@gmail.com>
2024-02-15 18:28:01 -0600

    done with exercise 3: Shell

--- josh_css_course/module_4/workshop/sole-and-ankle-main/src/components/ShoeIndex/ShoeIndex.js
+++ josh_css_course/module_4/workshop/sole-and-ankle-main/src/components/ShoeIndex/ShoeIndex.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS } from "../../constants";
 
-import Breadcrumbs from '../Breadcrumbs';
-import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
-import ShoeGrid from '../ShoeGrid';
+import Breadcrumbs from "../Breadcrumbs";
+import Select from "../Select";
+import Spacer from "../Spacer";
+import ShoeSidebar from "../ShoeSidebar";
+import ShoeGrid from "../ShoeGrid";
 
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
     <Wrapper>
+      <LeftColumn>
+        <Breadcrumbs>
+          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+        </Breadcrumbs>
+        <Spacer size={42} />
+        <ShoeSidebar />
+      </LeftColumn>
       <MainColumn>
         <Header>
           <Title>Running</Title>
@@ -27,32 +36,42 @@ const ShoeIndex = ({ sortId, setSortId }) => {
         <Spacer size={34} />
         <ShoeGrid />
       </MainColumn>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  // border: 3px dashed red;
+  display: flex;
 
-const LeftColumn = styled.div``;
+  & > * {
+    // border: 5px dashed purple;
+  }
+`;
 
-const MainColumn = styled.div``;
+const LeftColumn = styled.div`
+  padding-top: 10px;
+  flex: 1;
+`;
 
-const Header = styled.header``;
+const MainColumn = styled.div`
+  flex: 4;
+`;
+
+const Header = styled.header`
+  // border: 3px dashed red;
+  display: flex;
+  align-items: center;
+
+  // & > * {
+  //   border: 5px dashed purple;
+  // }
+`;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+  margin-right: auto;
 `;
 
 export default ShoeIndex;
