4ea99bf548f1aece990cca2c335d40a75e434d5d
Jose Lopez Renteria <joselukas24@gmail.com>
2024-03-01 16:07:24 -0600

    done with the workshopt

--- josh_css_course/module_6/workshop/unsprinkle-main/src/components/Hero/Hero.js
+++ josh_css_course/module_6/workshop/unsprinkle-main/src/components/Hero/Hero.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      <HeroImage
+        alt="A sample of images found on the site - a cat looking straight ahead"
+        srcSet="
+        ../images/hero-img.avif 1x,
+        ../images/hero-img@2x.avif 2x,
+        ../images/hero-img@3x.avif 3x,
+        ../images/hero-img.jpg 1x,
+        ../images/hero-img@2x.jpg 2x,
+        ../images/hero-img@3x.jpg 3x,"
+      />
+      <Swoop alt="" src="/swoop.svg" />
     </Wrapper>
   );
 };
